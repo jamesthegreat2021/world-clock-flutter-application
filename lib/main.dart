0acfe18505d3bc82d05a84cec08d05002c72bffd
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-02 16:27:00 +0300

    addition of pages for more functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:world_app/pages/home.dart'; 
+import 'package:world_app/pages/loading.dart'; 
+import 'package:world_app/pages/choose_location.dart';
 
 void main(){
   runApp(
     MaterialApp(
-      home: Home(),
+      
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      }
     ),
   );
 }
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,9 +8,24 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  @override
+  //this runs when the function is first initialized
+  void initState() {
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Choose A location'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       body: Text('choose Location')
     );
   }
